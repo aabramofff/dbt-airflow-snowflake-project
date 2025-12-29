96633971383baa11bd5ffe4d8ec9a3e9aa46b85e
Artsiom Abramau <artsiom.abramau@innowise.com>
2025-12-29 13:39:57 +0100

    Feat: Added Effectivity Satellite into Business Vault

---
+++ dbt_core/models/business_vault/eff_customer_order.sql
@@ -0,0 +1,32 @@
+{{ config(
+    materialized='incremental',
+    tags=['business_vault']
+) }}
+
+WITH link_data AS (
+    SELECT 
+        order_hk,
+        customer_hk,
+        load_date,
+        record_source
+    FROM {{ ref('link_order_customer') }}
+)
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['order_hk', 'customer_hk', 'load_date']) }} AS eff_sat_hk,
+    order_hk,
+    customer_hk,
+    load_date AS start_date,
+    CAST('9999-12-31' AS DATE) AS end_date,
+    TRUE AS is_active,
+    'BUSINESS_LOGIC' AS record_source
+FROM link_data
+
+{% if is_incremental() %}
+    WHERE NOT EXISTS (
+        SELECT 1 
+        FROM {{ this }} AS t 
+        WHERE t.order_hk = link_data.order_hk 
+          AND t.customer_hk = link_data.customer_hk
+    )
+{% endif %}
--- dbt_core/models/marts/dim_date.sql
+++ dbt_core/models/marts/dim_date.sql
@@ -3,7 +3,7 @@
 ) }}
 
 WITH date_spine AS (
-  {{ date_spine(
+  {{ dbt_utils.date_spine(
       datepart="day",
       start_date="cast('1992-01-01' as date)",
       end_date="cast('1998-12-31' as date)"
